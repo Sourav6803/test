1ab24f65af64c7419add4c27cec9a2ba3d14947a
Sourav Bhukta <souravbhukta8@gmail.com>
2024-01-21 23:42:46 +0530

    server

--- src/app.ts
+++ src/app.ts
@@ -17,9 +17,5 @@ app.get("/test", (req:Request, res: Response, next:NextFunction)=> {
     res.status(200).json({success: true, message: "Welocome to LMS"})
 })
 
-app.all('*', (req:Request, res:Response, next:NextFunction)=>{
-    const err = new Error(`Route ${req.originalUrl} not found`) as any
-    err.statusCode = 404
-    next(err)
-})
+
 
